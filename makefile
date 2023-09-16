5b3bb45c424729d454b9f522cb9b4d2c557678ff
ApianbelleDev <miracleforcemanga@gmail.com>
2023-09-16 13:09:05 -0400

    gf saves the day~

--- makefile
+++ makefile
@@ -1,6 +1,39 @@
-all:
-	ca65 *.asm -o main.o 
-	ld65 *.o -o star-command.nes -t nes
-	Mesen star-command.nes
+# assembler/linker defines
+# flags
+ASM_FLAGS  = -g $(addprefix -I,${INC_PATHS})$(addprefix -D,${DEFINES})
+LINK_FLAGS = --dbgfile bin/${BIN_NAME}.dbg -C ${CFG_FILE} -m bin/${BIN_NAME}.txt
+# include paths
+INC_PATHS = src/inc/ # incbin seems not to be affected??
+# (??? type) constants for ca65
+DEFINES = ${CONFIG}
+# pad value is defined in the config file
+# cfg file
+CFG_FILE = nrom128.cfg
+
+# filename for the binary
+BIN_NAME = star-command
+
+# dependencies
+ASM_REQS = $(wildcard src/inc/*.inc)
+	# i wonder if i could set this one to only trip for a .65 that shares the .imp's name
+LINK_REQS = $(patsubst src/%.asm,obj/%.o,$(wildcard src/*.asm))
+GFX_REQS = 
+
+.PHONY: all clean
+
+all: bin/${BIN_NAME}.nes
+
 clean:
-	rm *.nes *.o
+	rm -rf bin/ obj/
+
+bin/:
+	mkdir bin/
+
+obj/:
+	mkdir obj/
+
+obj/%.o: src/%.asm $(ASM_REQS) $(GFX_REQS) obj/ 
+	ca65 ${ASM_FLAGS} -o $@ $<
+
+bin/${BIN_NAME}.nes: $(LINK_REQS) bin/
+	ld65 ${LINK_FLAGS} -o $@ obj/*.o
--- src/init.asm
+++ src/init.asm
@@ -1,7 +1,9 @@
-.segment "HEADER"
-	.byte "NES" 			; string required for iNES header
+.include "inc/global.inc"
+
+.segment "INESHDR"
+	.byte "NES" 		; string required for iNES header
 	.byte $1a				; signature of iNES header that the emulator will look for
-	.byte $02 				; 2 * 16KB PRG ROM
+	.byte $01 				; 16KB PRG ROM
 	.byte $01  				; 1 * 8KB CHR ROM
 	.byte %00000000 		; mapper and mirroring. none yet
 	.byte $0
@@ -9,10 +11,11 @@
 	.byte $0
 	.byte $0
 	.byte $0, $0, $0, $0 	; unused. 
-	.segment "ZEROPAGE"
-		
-	.segment "STARTUP"
-	.segment "CODE"
+.segment "ZEROPAGE"
+.segment "STACK"
+.segment "SHADOWOAM"
+.segment "STARTUP"
+.segment "CODE"
 
 Reset:
     sei        ; ignore IRQs
@@ -63,48 +66,8 @@ Reset:
 @vblankwait2:
     bit $2002
     bpl @vblankwait2
-main:
-; simple code for testing audio, since the NES can't easily print text. this is basically the 2A03
-; equivalent to a C "Hello, World" program :3 
-	lda #$01	; square 1
-	sta $4015
-	lda #$08	; period low
-	sta $4002
-	lda #$02	; period high
-	sta $4003
-	lda #$bf	; volume
-	sta $4000
-
-	lda $2002    ; read PPU status to reset the high/low latch to high
-	lda #$3F
-	sta $2006    ; write the high byte of $3F10 address
-	lda #$10
-	sta $2006    ; write the low byte of $3F10 address
-	
-	ldx #$00
-	LoadPalettes:
-	lda PaletteData, x      	                          
-	sta $2007               
-	inx                     
-	cpx #$20               
-	bne LoadPalettes 
-		     
-	ldx #$00
-	LoadSprites:
-	lda SpriteData, x		
-	sta $0200, x
-	inx
-	cpx #$30 ;(4 bytes per sprite, 1 sprite *for now*)
-	bne LoadSprites
-			
-	; re-enable interrupts
-	lda #%10000000   ; enable NMI, sprites from Pattern Table 0
-	sta $2000
-		
-	lda #%00010000   ; enable sprites
-	sta $2001
-forever:
-	jmp forever
+mainLoop:
+	jmp main
 NMI:
 	lda #$00
 	sta $2003 	; set the low byte (00) of the RAM address
@@ -116,14 +79,13 @@ IRQ:
 PaletteData:
 	.byte $0e, $27, $10, $30,  $0f, $0f, $0f, $0f,  $0f, $0f, $0f, $0f,  $0f, $0f, $0f, $0f ; background palette data
 	.byte $0e, $00, $10, $30,  $0f, $0f, $0f, $0f,  $0f, $0f, $0f, $0f,  $0f, $0f, $0f, $0f ; sprite palette data
-SpriteData:
-	; y, tile num, attributes, x
-
-	; tile 0
-	.byte $80, $07, $00,$80
+	SpriteData:
+		; y, tile num, attributes, x
+		; tile 0
+		.byte $80, $07, $00,$80
 .segment "VECTORS"
     .word  NMI
     .word  Reset
     .word  0
-.segment "CHARS"
+.segment "CHR"
 	.incbin "tiles.chr"
